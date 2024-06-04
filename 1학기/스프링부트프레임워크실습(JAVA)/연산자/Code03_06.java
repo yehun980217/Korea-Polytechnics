df3d206d161142f103a7c144d2cc0df84aa8a053
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-04 20:20:53 +0900

    제곱 연산(Math 클래스)

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/연산자/Code03_06.java
@@ -0,0 +1,19 @@
+public class Code03_06 {
+    public static void main(String[] args) {
+        double num;
+        num = Math.pow(3, 2); // Math.pow(밑수, 지수) 3²
+        System.out.println(num);
+        num = Math.pow(4, 3); // 4³
+        System.out.println(num);
+
+        /* 자바 프로그래밍을 하다 보면 수학과 관련된 계산을 해야 하는 경우가 종종 있습니다.
+           간단한 사칙연산은 지금까지 배운 내용으로 쉽게 해결할 수 있지만
+           제곱, 제곱근, 최댓값, 최솟값, 삼각함수 등의 복잡한 계산은 코드로 풀어내기가 어렵습니다.
+           그래서 자바는 다음과 같이 다양한 수학 계산 메서드가 포함된 Math 클래스를 제공하고 있습니다.
+           Math.abs(): 절댓값
+           Math.log(): 로그값
+           Math.max(): 최댓값
+           Math.min(): 최솟값
+           Math.sin(), Math.cos(), Math.tan(): 삼각함수 */
+    }
+}
