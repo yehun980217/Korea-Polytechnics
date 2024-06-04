2b01542658b0972bae1c72410327f3b11265818c
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-04 20:07:44 +0900

    기본 산술 연산자

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/연산자/Code03_01.java
@@ -0,0 +1,23 @@
+public class Code03_01 {
+    public static void main(String[] args) {
+        int n1, n2, res;
+        n1 = 5;
+        n2 = 3;
+
+        // = 기준으로 오른쪽의 값을 왼쪽에 기입
+        res = n1 + n2;
+        System.out.println(res);
+
+        res = n1 - n2;
+        System.out.println(res);
+
+        res = n1 * n2;
+        System.out.println(res);
+
+        res = n1 / n2;
+        System.out.println(res);
+
+        res = n1 % n2;
+        System.out.println(res);
+    }
+}
