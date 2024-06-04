8c3952a01d52f11c69ab4df1438ca595612e0f7b
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-04 13:28:08 +0900

    Delete 1학기/스프링부트프레임워크실습(JAVA)/Code02_03.java

--- 1학기/스프링부트프레임워크실습(JAVA)/Code02_03.java
+++
@@ -1,9 +0,0 @@
-public class Code02_03 {
-    public static void main(String[] args) {
-        int a = 100; // 정수형 변수 a에 100 기입
-        int b = 200; // 정수형 변수 b에 200 기입
-        int c = a + b; // a(100)와 b(200)을 더한 값인 300을 c에 기입
-        System.out.println("c의 값 ==> " + c); // c의 값을 출력하는 코드
-        System.out.println(a + " + " + b + " = " + c); // 전체 계산식이 출력되는 코드
-    }
-}
