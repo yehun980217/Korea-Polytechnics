9c4f81278df4c4025eed453ee6a2f5adf424e1b2
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-11 12:02:03 +0900

    문자열 안에 따옴표 표현

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/데이터형과 문자열/Code04_07.java
@@ -0,0 +1,10 @@
+public class Code04_07 {
+    public static void main(String[] args) {
+        // 백슬래시 다음의 작은따옴표나 큰따옴표는 문자로 인식됨
+        String str1 = "작은따옴표는 \' 모양입니다.";
+        String str2 = "큰따옴표는 \" 모양입니다.";
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
