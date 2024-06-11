817ef69c2ffc1fcbadc5201a05e9933e9040f4f8
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-11 11:33:19 +0900

    불 데이터형

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/데이터형과 문자열/Code04_04.java
@@ -0,0 +1,11 @@
+public class Code04_04 {
+    public static void main(String[] args) {
+        boolean boo1, boo2;
+
+        boo1 = true;
+        System.out.println(boo1); // true
+
+        boo2 = (100 < 10);
+        System.out.println(boo2); // false
+    }
+}
