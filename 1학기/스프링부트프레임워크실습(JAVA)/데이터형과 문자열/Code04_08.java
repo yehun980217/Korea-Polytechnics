90870afc75cc7b4c0b9d389fc165503a52a44f6a
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-11 12:03:39 +0900

    여러 행의 문자열 만들기

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/데이터형과 문자열/Code04_08.java
@@ -0,0 +1,8 @@
+public class Code04_08 {
+    public static void main(String[] args) {
+        // 더하기(+) 연산자로 연결하면 여러 행을 문자열로 만들 수 있음
+        // \n을 사용하면 행갈이됨
+        String str = "난생처음 \n" + "자바를 \n" + "열공 중입니다.";
+        System.out.println(str);
+    }
+}
