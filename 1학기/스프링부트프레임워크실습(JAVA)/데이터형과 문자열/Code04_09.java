93ff7213d9d6318171c4b84aa8116040c4710a94
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-11 12:06:20 +0900

    이스케이프 문자를 사용한 문자열

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/데이터형과 문자열/Code04_09.java
@@ -0,0 +1,16 @@
+public class Code04_09 {
+    public static void main(String[] args) {
+        System.out.println("\n줄바꿈\n연습");
+        System.out.println("\t탭키\t연습");
+        System.out.println("어떤 글자를 \"강조\"하는 효과1");
+        System.out.println("어떤 글자를 \'강조\'하는 효괴2");
+        System.out.println("\\\\ 백슬래시 2개 출력");
+
+        /* 줄바꿈
+           연습
+                탭키	연습
+           어떤 글자를 "강조"하는 효과1
+           어떤 글자를 '강조'하는 효괴2
+           \\ 백슬래시 2개 출력 */
+    }
+}
