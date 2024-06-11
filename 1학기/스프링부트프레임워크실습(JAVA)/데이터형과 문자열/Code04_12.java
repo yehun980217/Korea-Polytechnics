c128a599164e9da347421489a9edf53a13afa1fb
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-11 12:17:55 +0900

    문자열 관련 메서드 toUpperCase( ), toLowerCase( )

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/데이터형과 문자열/Code04_12.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class Code04_12 {
+    public static void main(String[] args) {
+        String str = "Fisrt Java Study";
+        String uStr, lStr;
+
+        uStr = str.toUpperCase();
+        System.out.println(uStr); // 출력 : FISRT JAVA STUDY
+
+        lStr = str.toLowerCase();
+        System.out.println(lStr); // 출력 : fisrt java study
+    }
+}
