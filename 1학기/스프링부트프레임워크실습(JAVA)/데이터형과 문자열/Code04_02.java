702b46b4351661fc0da97246718d1a367439cb17
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-11 11:31:50 +0900

    실수 데이터형

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/데이터형과 문자열/Code04_02.java
@@ -0,0 +1,9 @@
+public class Code04_02 {
+    public static void main(String[] args) {
+        // float형과 double형의 정밀도 비교
+        float f = 0.1234567890123456789012345f;
+        double d = 0.1234567890123456789012345;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
