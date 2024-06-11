d49861614273bc24456e1019a9aa0892ef705de6
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-11 11:37:59 +0900

    System.out.printf( ) 사용법

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/데이터형과 문자열/Code04_05.java
@@ -0,0 +1,9 @@
+public class Code04_05 {
+    public static void main(String[] args) {
+        System.out.printf("200+300");
+        System.out.println();
+        System.out.printf("%d", 200+300); // %d는 정수를 의미하며 이러한 것을 서식이라한다.
+        System.out.println();
+        System.out.printf("%d %d", 100, 200); // %d가 여러 개인 경우 짝이 맞아야한다.
+    }
+}
