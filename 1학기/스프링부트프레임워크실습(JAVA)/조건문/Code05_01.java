50a7a4b504b7c7e3e35959f04dafcc84fe178b43
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-14 13:56:28 +0900

    기본 if문

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/조건문/Code05_01.java
@@ -0,0 +1,9 @@
+public class Code05_01 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) { // num이 100보다 작다면~
+            System.out.println("100보다 작습니다."); // 해당 코드를 수행
+        }
+    }
+}
