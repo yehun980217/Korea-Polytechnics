06a52d144ee1c4c2aff8ef27fb1b438e4d7c7ca4
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-14 13:58:54 +0900

    if~else문

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/조건문/Code05_03.java
@@ -0,0 +1,11 @@
+public class Code05_03 {
+    public static void main(String[] args) {
+        int num = 200;
+
+        if (num < 100) { // num이 100보다 작다면~
+            System.out.println("100보다 작습니다."); // num이 100보다 작은 게 참인 경우 해당 코드 수행
+        } else { // num이 100보다 작지않다면~
+            System.out.println("100보다 큽니다."); // num이 100보다 작지않다면 즉 거짓인 경우 해당 코드 수행
+        }
+    }
+}
