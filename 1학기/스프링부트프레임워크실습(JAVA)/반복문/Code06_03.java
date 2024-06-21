2b61163780b382b43ff2b55e9ac81bd92cb62b7b
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-06-21 20:17:53 +0900

    for문의 개요

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/반복문/Code06_03.java
@@ -0,0 +1,11 @@
+public class Code06_03 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) { // 조건식에 사용된 i를 코드 내부에서 사용해보기
+            System.out.println(i + ": 난생처음 자바는 재미있습니다.^^");
+        }
+
+        for (int i = 1; i <= 10; i ++) { // i를 이용하여 1부터 10까지 출력
+            System.out.print(i + " ");
+        }
+    }
+}
