176828571d541bb372525d009f23f3d17c38adb8
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-07-03 12:06:46 +0900

    while문의 형식

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/반복문/Code06_10.java
@@ -0,0 +1,9 @@
+public class Code06_10 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<3) { // i가 3보다 작을 때 까지 반복
+            System.out.println(i + " : 난생처음 자바는 재미있습니다. ^^");
+            i++;
+        }
+    }
+}
