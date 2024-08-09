3406feda64d3fc6260e0a60de9f3616c6c1d495b
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-08-09 09:52:31 +0900

    for문의 다른 용법

---
+++ 1학기/스프링부트프레임워크실습(JAVA)/배열/Code09_07.java
@@ -0,0 +1,8 @@
+public class Code09_07 {
+    public static void main(String[] args) {
+        int[] ary = {10, 20, 30};
+        for (int data : ary) { // 편리하게 배열에 접근할 수 있도록 for (변수 : 배열) 형식의 구문을 사용할 수 있음
+            System.out.println(data);
+        }
+    }
+}
