dd312d4c203c6406652c47bb2e9862005941361a
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2025-04-09 10:41:24 +0900

    Add files via upload

---
+++ 2학년 1학기/융합프로젝트실습1/JPA실습/src/main/java/kopo/poly/SpringJpaJdk17TemplateApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaJdk17TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaJdk17TemplateApplication.class, args);
+    }
+
+}
